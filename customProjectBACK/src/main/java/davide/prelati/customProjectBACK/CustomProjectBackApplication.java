6b81812adce7f8385253f0af6c95ef3711137738
DavidePrelati <davide.prelati@gmail.com>
2024-07-29 10:08:38 +0200

    first commit

---
+++ customProjectBACK/src/main/java/davide/prelati/customProjectBACK/CustomProjectBackApplication.java
@@ -0,0 +1,13 @@
+package davide.prelati.customProjectBACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomProjectBackApplication.class, args);
+	}
+
+}
---
+++ customProjectBACK/src/test/java/davide/prelati/customProjectBACK/CustomProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package davide.prelati.customProjectBACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
